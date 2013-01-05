0d3b3ae5d89bf0ceff8ee31ccc225513883b6565
Roopak Venkatakrishnan <roopak.v+commits@gmail.com>
2013-01-04 22:44:27 -0500

    Base project
    all the base files for the project

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: Roopak
+ *
+ * Created on January 4, 2013, 10:05 PM
+ */
+
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    cout<<"Hello World";
+    return 0;
+}
+
